ef35932af57cb8029fcc517e28f16ba7c5b481f0
Leviano1 <3014673k@student.gla.ac.uk>
2026-07-23 22:57:20 +0100

    Tutorial progress: 46:21

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css'
 import MovieCard from "./components/MovieCard"
+import Home from "./pages/Home"
 
 function App() { {/*component - any function in JS that returns some kind of JSX code*/}
   const movieNumber = 1;
   return (
     <> {/*fragmentwraps different components so that we can return them*/}
-    {/*if statement block*/}
-      {movieNumber === 1 ? ( 
-        <MovieCard movie={{title: "Tim's Film", release_date:"2024"}}/>
-      ) : (
-       <MovieCard movie={{title: "Tim's Film", release_date:"2020"}}/>
-      )}
+      <Home />
     </>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,47 @@
 {/*../ because we need to go back one level*/}
 import MovieCard from "../components/MovieCard"
+import { useState } from "react";
 
 function Home(){
+    const [searchQuery, setSearchQuery] = useState("");
+    /*searchQuery - defines the state, setSearchQuery - a function to update the state */
+
     const movies = [
         {id: 1, title: "John Wick", release_date: "2020"},
         {id: 1, title: "Terminator", release_date: "1999"},
         {id: 1, title: "The Matrix", release_date: "1998"},
-    ]
+    ];
 
-    return <div className="home">
+    const handleSearch = (e) => {
+        e.preventDefault();
+        /*text typed in the textbox stays, withought getting refreshed.*/
+        alert(searchQuery)
+    };
+
+    return (
+    <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input 
+                    type="text" 
+                    placeholder="Search for movies..." 
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    /*update the state form the input element*/
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
         <div className="movies-grid">
-            {movies.map((movie) => (
-                <MovieCard movie={movie} key={movie.id} />
+            {movies.map(
+                (movie) => (
+                    movie.title.toLowerCase().startsWith(searchQuery) && (
+                        <MovieCard movie={movie} key={movie.id} />
+                /*.map function iterates through all our mivies values*/
+                )
             ))}
         </div>
     </div>
+    );
 }
+
+export default Home
