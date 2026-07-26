3927ec11f07722555d05ed867e7e68291211e439
Leviano1 <3014673k@student.gla.ac.uk>
2026-07-26 23:27:17 +0100

    Finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,22 @@ import './css/App.css';
 import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
 import {Routes, Route} from "react-router-dom";
+import { MovieProvider } from './contexts/MovieContext';
 import NavBar from "./components/NavBar";
 
 function App() { {/*component - any function in JS that returns some kind of JSX code*/}
   return (
-    <div>
-      <NavBar />
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/favorites" element={<Favorites />}/>
-        </Routes>
-      </main>
-    </div>
+    <MovieProvider>
+      <div>
+        <NavBar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/favorites" element={<Favorites />}/>
+          </Routes>
+        </main>
+      </div>
+    </MovieProvider>
   );
 }
 
